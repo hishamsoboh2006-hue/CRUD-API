3ef3c8c2953eea0b9ddf9b3dd7e73e2a7e3fc29d
Hisham Soboh <hishamsoboh2006@gmail.com>
2026-08-26 15:13:03 +0300

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 const  app = express();
 const PORT = 3000;
+const tasks=[
+{
+id:1,
+title:"learn",
+done:true
+},
+{
+id:2,
+title:"build",
+done:false
+},
+{
+id:3,
+title:"test",
+done:true
+}
+];
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   console.log('[GET ROUTE]');
@@ -11,5 +28,19 @@ app.get("/", (req, res) => {
 app.get("/health", (req,res)=>{
   res.json({status:"ok"});
 });
+app.get("/tasks" , (req,res)=>{
+  res.json(tasks);
+});
+app.get("/tasks/:id", (req, res) => {
+ const id=Number(req.params.id);
+ const task=tasks.find(task=>task.id===id);
+if (!task) {
+  return res.status(404).json({
+    error: `Task ${id} not found`
+  });
+}
+res.json(task);
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
